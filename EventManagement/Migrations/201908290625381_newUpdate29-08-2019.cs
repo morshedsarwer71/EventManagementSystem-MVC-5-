1f01623deccd906e3693a2e6ac662f2de2225c08
Morshed Sarwer <morshedsarwer@hotmail.com>
2019-08-29 12:30:14 +0600

    upter migration after 29-08-2019

---
+++ EventManagement/Migrations/201908290625381_newUpdate29-08-2019.cs
@@ -0,0 +1,16 @@
+namespace EventManagement.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class newUpdate29082019 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
